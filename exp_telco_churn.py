06b65b882e7787cf81678ad4fd1dfccd34129b93
Oliver k Taylor <o.k.p.taylor@gmail.com>
2023-04-30 23:47:47 -0500

    added exp_telco_churn.py

---
+++ exp_telco_churn.py
@@ -0,0 +1,53 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+col_cat = []
+
+#This function runs through the listed cat ver plots them and returns the hypo
+def cat_graph_loop(dataframe_train_name, col_cat, target_ver, target_ver_column_name):
+    for col in col_cat:
+        print()
+        print(col.upper())
+        print(dataframe_train_name[col].value_counts())
+        print(dataframe_train_name[col].value_counts(normalize=True))
+        dataframe_train_name[col].value_counts().plot.bar()
+        plt.show()
+        print()
+        print()
+        print(f'HYPOTHESIZE')
+        print(f"H_0: {col.lower().replace('_',' ')} does not affect {target_ver}")
+        print(f"H_a: {col.lower().replace('_',' ')} affects {target_ver}")
+        print()
+        print(f'VISUALIZE')
+        sns.barplot(x=dataframe_train_name[col], y=dataframe_train_name[target_ver_column_name])
+        plt.title(f"{col.lower().replace('_',' ')} vs {target_ver}")
+        plt.show()
+        print()
+        print('ANALYZE and SUMMARIZE')
+        observed = pd.crosstab(dataframe_train_name[col], dataframe_train_name[target_ver_column_name])
+        chi2Test(observed)
+        print()
+        print()
+#the call should be: prep.cat_graph_loop(dataframe_train_name, col_cat, "target_ver", "target_ver_column_name")        
+
+#this funciton works in this module to run the chi2 test with the above function
+def chi2Test(observed):
+    alpha = 0.05
+    chi2, pval, degf, expected = stats.chi2_contingency(observed)
+    print('Observed')
+    print(observed.values)
+    print('\nExpected')
+    print(expected.astype(int))
+    print('\n----')
+    print(f'chi^2 = {chi2:.4f}')
+    print(f'p-value = {pval:.4f}')
+    print('----')
+    if pval < alpha:
+        print ('We reject the null hypothesis.')
+    else:
+        print ("We fail to reject the null hypothesis.")
+# prep.chi2Test(observed) is the call 
